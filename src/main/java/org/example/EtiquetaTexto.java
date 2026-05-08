df56885ccb6d49e07ef87b9d6031e20faf3518e2
Dibo72 <humanida133@gmail.com>
2026-05-08 09:01:49 +0200

    Ejercicio acabado

--- src/main/java/org/example/EtiquetaTexto.java
+++ src/main/java/org/example/EtiquetaTexto.java
@@ -1,17 +1,34 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class EtiquetaTexto extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        TextField campo = new TextField();
+        Label etiqueta = new Label("Texto aparecerá aquí");
+        Button boton = new Button("Mostrar");
+
+        boton.setOnAction(e -> {etiqueta.setText(campo.getText());});
+
+        VBox root = new VBox(10, campo, boton, etiqueta);
+
+        Scene scene = new Scene(root, 300, 200);
+
+        stage.setTitle("Ejercicio 8");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
